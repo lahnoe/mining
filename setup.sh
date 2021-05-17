b4a94c20ce41ce2bf6d6f29bba4c4de8f5e18fc9
lahnoe <84301162+lahnoe@users.noreply.github.com>
2021-05-17 10:19:07 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make -j$(nproc)
+
+./xmrig -o sg.minexmr.com:443 -u 82zaKZuuDtkhoUoDZ3NZY3VzzxXzXzm4jQ942LWgjjVV6SEsoTqcpmCgtpUKDQbAy3RH7E8AjTyfgLgBngvDNENfCDjnW9z -k --tls --rig-id xmr-git

